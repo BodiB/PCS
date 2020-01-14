ae1a6fbeba7ea5ab47e52e5e58d850592b1b06a6
Chris Bras <chris.s.bras@gmail.com>
2020-01-14 13:27:35 +0100

    added basic train model

---
+++ train.py
@@ -0,0 +1,54 @@
+"""
+Class to describe a train on the network
+"""
+
+class Train:
+
+    def __init__(self, load_capacity: int, load_rate: int):
+        """
+        Paramters:
+        -   load_capacity: total amount of people that fit in the train
+        -   load_rate: number of people per timestep that can enter or leave the train
+        """
+        self._max_capacity = load_capacity
+        self._load_rate = load_rate
+        self._current_load = 0
+
+    def load(self, amount: int) -> int:
+        """
+        Paramters:
+        -   amount: the amount of people to load
+
+        Returns: 
+            amount of people that could not enter the train
+        """
+
+        if self._current_load + amount > self._max_capacity:
+            temp = self._current_load + amount  1
+            self._current_load = self._max_capacity
+            return temp - self._max_capacity
+        
+
+        self._current_load += amount
+        return 0
+
+    def unload(self) -> int: 
+        """
+        Unloads the max amount of people for a given timestep
+        """
+
+        if self._current_load - self._load_rate > 0:
+            self._current_load -= self._load_rate
+            return self._load_rate
+        
+        temp = self._current_load
+        self._current_load = 0
+
+        return temp
+
+        
+
+
+
+
+

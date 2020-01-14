dec597f6c76e66d8275fb81fbd9f88be4f01ee71
Chris Bras <chris.s.bras@gmail.com>
2020-01-14 14:52:15 +0100

    added requirements

--- simulation.py
+++ simulation.py
@@ -1,6 +1,20 @@
+import cv2
+import numpy as np
+
+
 from train import Train
 from railway import Railway
+from station import Station
 
+class Simulation:
 
+    def __init__(self): 
+        self.stations = [
+            Station(1000),
+            Station(3200),
+            Station(10000)
+        ]
 
-
+    
+    def start(self):
+        

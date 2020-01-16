d4d07e8338589ced2f80e9b78030ae71cd138e9c
Chris Bras <chris.s.bras@gmail.com>
2020-01-16 15:29:55 +0100

    added correct time to zandvoort departure

--- data/timesheets.py
+++ data/timesheets.py
@@ -1,5 +1,5 @@
 from traject import TimeSlot, Traject
 
 timeslots = [
-    [("Zandvoort aan zee", 4, 1), ("Overveen", 11, 11), ("Haarlem", 15, 16), ("Haarlem Spaarnwoude", 19, 19), ("Halfweg-Zwanenburg", 24, 24), ("Amsterdam Sloterdijk", 29, 29), ("Amsterdam Centraal", 35, -1)]
+    [("Zandvoort aan zee", 4, 4), ("Overveen", 11, 11), ("Haarlem", 15, 16), ("Haarlem Spaarnwoude", 19, 19), ("Halfweg-Zwanenburg", 24, 24), ("Amsterdam Sloterdijk", 29, 29), ("Amsterdam Centraal", 35, -1)]
 ]

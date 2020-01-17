4fc30ccd0757e555245e73cce75c4970bcffdf8f
artemiscapari <artemiscapari@gmail.com>
2020-01-17 15:44:36 +0100

    rijdende trein, hoofddorp-amsterdam

--- data/timesheets.py
+++ data/timesheets.py
@@ -2,5 +2,9 @@ from traject import TimeSlot, Traject
 
 timeslots = [
     [("Zandvoort aan zee", -1, 4), ("Overveen", 11, 11), ("Haarlem", 15, 16), ("Haarlem Spaarnwoude", 19, 19), ("Halfweg-Zwanenburg", 24, 24), ("Amsterdam Sloterdijk", 29, 29), ("Amsterdam Centraal", 35, -1)],
-    [("Zandvoort aan zee", -1, 34), ("Overveen", 41, 41), ("Haarlem", 45, 46), ("Haarlem Spaarnwoude", 49, 49), ("Halfweg-Zwanenburg", 54, 54), ("Amsterdam Sloterdijk", 59, 59), ("Amsterdam Centraal", 5, -1)]
+    [("Zandvoort aan zee", -1, 34), ("Overveen", 41, 41), ("Haarlem", 45, 46), ("Haarlem Spaarnwoude", 49, 49), ("Halfweg-Zwanenburg", 54, 54), ("Amsterdam Sloterdijk", 59, 59), ("Amsterdam Centraal", 5, -1)],
+    [("Hoofddorp", -1, 4), ("Schiphol Airport", 9, 11), ("Amsterdam Lelylaan", 17, 17), ("Amsterdam Sloterdijk", 21,23), ("Amsterdam Centraal", 29, -1)], 
+    [("Hoofddorp", -1, 25), ("Schiphol Airport", 29, 30), ("Amsterdam Lelylaan", 37, 37), ("Amsterdam Sloterdijk", 41,41), ("Amsterdam Centraal", 48, -1)], 
+    [("Hoofddorp", -1, 34), ("Schiphol Airport", 39, 41), ("Amsterdam Lelylaan", 47, 47), ("Amsterdam Sloterdijk", 51,53), ("Amsterdam Centraal", 59, -1)], 
+    [("Hoofddorp", -1, 55), ("Schiphol Airport", 59, 60), ("Amsterdam Lelylaan", 7, 7), ("Amsterdam Sloterdijk", 11,11), ("Amsterdam Centraal", 18, -1)], 
 ]

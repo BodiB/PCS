092c5f5f2816943726546d6e1dec082fc8771d47
Bodi Boele <bodiboele@gmail.com>
2020-01-20 16:59:48 +0100

    456

--- data/timesheets.py
+++ data/timesheets.py
@@ -29,8 +29,8 @@ timeslots = [
     # Amsterdam Centraal - Baarn (Momenteel in de actieve 4 en 34 sprinters Hoofddorp-Amsterdam Centraal verwerkt, behalve de eerste.)
     [("Amsterdam Centraal", -1, 11), ("Amsterdam Muiderpoort", 17, 17), ("Amsterdam Science Park", 19, 19), ("Diemen", 22, 22), ("Weesp", 27,
                                                                                                                                  29), ("Naarden-Bussum", 35, 35), ("Bussum Zuid", 38, 38), ("Hilversum Media Park", 42, 42), ("Hilversum", 44, 45), ("Baarn", 51, -1)],
-    [("Amsterdam Centraal", -1, 41), ("Amsterdam Muiderpoort", 46, 46), ("Amsterdam Science Park", 49, 49), ("Diemen", 52, 52), ("Weesp",
-                                                                                                                                 57, 59), ("Naarden-Bussum", 5, 5), ("Bussum Zuid", 8, 8), ("Hilversum Media Park", 12, 12), ("Hilversum", 14, 15), ("Baarn", 21, -1)],
+    # [("Amsterdam Centraal", -1, 41), ("Amsterdam Muiderpoort", 46, 46), ("Amsterdam Science Park", 49, 49), ("Diemen", 52, 52), ("Weesp",
+    #                                                                                                                              57, 59), ("Naarden-Bussum", 5, 5), ("Bussum Zuid", 8, 8), ("Hilversum Media Park", 12, 12), ("Hilversum", 14, 15), ("Baarn", 21, -1)],
     # SPR Leiden Centraal (Vanaf Nieuw Vennep) - Hoorn Kersenboogerd
     [("Nieuw Vennep", -1, 4), ("Hoofddorp", 9, 9), ("Schiphol Airport", 13, 15), ("Amsterdam Lelylaan", 22, 22), ("Amsterdam Sloterdijk", 26, 28), ("Zaandam", 34, 34),
      ("Zaandam Kogerveld", 38, 38), ("Purmerend Weidevenne", 44, 44), ("Purmerend", 47, 47), ("Purmerend Overwhere", 50, 50), ("Hoorn", 1, 3), ("Hoorn Kersenboogerd", 6, -1)],

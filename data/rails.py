0c16afe66d7ee30cee4c5019620769a40d089061
Chris Bras <chris.s.bras@gmail.com>
2020-01-22 13:05:37 +0100

    fixed a few times

--- data/rails.py
+++ data/rails.py
@@ -3,10 +3,10 @@
 rail_list = [
     ("Zandvoort aan zee", "Overveen", 6.3, 100),
     ("Overveen", "Haarlem", 2, 100),
-    ("Haarlem", "Haarlem Spaarnwoude", 2.6, 100),
-    ("Haarlem Spaarnwoude", "Halfweg-Zwanenburg", 5, 100),
-    ("Halfweg-Zwanenburg", "Amsterdam Sloterdijk", 6.4, 100),
-    ("Amsterdam Sloterdijk", "Amsterdam Centraal", 4.6, 100),
+    ("Haarlem", "Haarlem Spaarnwoude", 2.6, 130),
+    ("Haarlem Spaarnwoude", "Halfweg-Zwanenburg", 5, 130),
+    ("Halfweg-Zwanenburg", "Amsterdam Sloterdijk", 6.4, 130),
+    ("Amsterdam Sloterdijk", "Amsterdam Centraal", 4.6, 130),
 
     ("Hoofddorp", "Schiphol Airport", 4.7, 100),
     ("Schiphol Airport", "Amsterdam Lelylaan", 8.3, 100),

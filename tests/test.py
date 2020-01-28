702304f31988d36f10afa4eaad5417fcb0605551
Bodi Boele <bodiboele@gmail.com>
2020-01-28 21:24:41 +0100

    calculate the sums of the tests

--- tests/test.py
+++ tests/test.py
@@ -3,16 +3,19 @@ import csv
 import numpy as np
 
 if __name__ == "__main__":
-    with open("SD10.csv") as fin:
-        total = 0
-        delays = 0
-        for row in csv.reader(fin):
-            # print(row)
-            if row[0] == "NAME":
-                print("skip")
-            else:
-                # print(int(row[1]))
-                total += int(row[1])
-                delays += int(row[2])
-        print(total)
-        print(delays)
+    filenames = ["CS10.csv", "HLM10.csv", "LD10.csv", "SD10.csv"]
+    for filename in filenames:
+        with open(filename) as fin:
+            total = 0
+            delays = 0
+            for row in csv.reader(fin):
+                # print(row)
+                if row[0] == "NAME":
+                    print(f"{filename}")
+                else:
+                    # print(int(row[1]))
+                    total += int(row[1])
+                    delays += int(row[2])
+            print(f"Total: {total}")
+            print(f"Delays: {delays}")
+            print("=================")
